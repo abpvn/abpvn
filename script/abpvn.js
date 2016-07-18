c49207106697a8a5f52302582dc21d0762cb52bd
Hoàng Nguyễn Đông <Hong Nguyn ng>
2016-07-18 12:09:31 +0700

    update text

--- script/abpvn.js
+++ script/abpvn.js
@@ -10,7 +10,7 @@
 // @run-at      document-end
 // @include     http://*
 // @include     https://*
-// @version     2.1.3.1
+// @version     2.1.3.2
 // @grant       none
 // ==/UserScript==
 /* String Prototype */
@@ -168,7 +168,7 @@ var fixSite={
           form.submit();
           var div = document.createElement('div');
           div.style="position: fixed; width: 100%; height: 100%; background: rgba(0,0,0,0.80); z-index: 999999999; text-align: center; color: white; top: 0; left: 0;";
-          div.innerHTML="<h1>Popup Adblock by ABPVN</h1>";
+          div.innerHTML="<h1>ABPVN Auto download is worked..</h1>";
           document.body.insertBefore(div,document.body.firstChild);
         }
       }
