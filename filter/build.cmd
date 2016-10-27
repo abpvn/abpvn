228cace04a77907a20804dffc50170a0cfff9e86
ABVPN <donghoang.nguyen@outlook.com>
2016-10-27 14:33:02 +0700

    update build script

--- filter/build.cmd
+++ filter/build.cmd
@@ -1,3 +1,7 @@
-echo off;
-copy /y /b src\abpvn_title.txt+src\abpvn_general.txt+src\abpvn_elemhide.txt+src\abpvn_whitelist.txt+src\abpvn_whitelist_elemhide.txt abpvn.txt
-copy /y /b src\abpvn_title.txt+src\abpvn_general.txt+src\abpvn_whitelist.txt abpvn_noelemhide.txt
+@echo off;
+rem Make time stamp update
+powershell -Command "(Get-Content src\abpvn_title.txt).replace(\"_time_stamp_\",(Get-Date -format \"dd MMMM yyyy HH:mm:ss\")) | Set-Content abpvn_title_tmp.txt -Encoding UTF8"
+rem Build file
+copy /y /b abpvn_title_tmp.txt+src\abpvn_general.txt+src\abpvn_elemhide.txt+src\abpvn_whitelist.txt+src\abpvn_whitelist_elemhide.txt abpvn.txt
+copy /y /b abpvn_title_tmp.txt+src\abpvn_general.txt+src\abpvn_whitelist.txt abpvn_noelemhide.txt
+DEL abpvn_title_tmp.txt
