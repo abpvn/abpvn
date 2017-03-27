22475827d7603f8c446ee5b8d9537c79fc038dcc
ABVPN <donghoang.nguyen@outlook.com>
2017-03-27 11:25:30 +0700

    Update or Fix [optimize filter]

--- filter/build-push.cmd
+++ filter/build-push.cmd
@@ -1,6 +1,4 @@
 echo off
-rem Update first
-git pull
 rem Soft filter rule and get Unique
 powershell -Command "copy src\abpvn_general.txt src\abpvn_general.tmp;copy src\abpvn_elemhide.txt src\abpvn_elemhide.tmp;copy src\abpvn_whitelist.txt src\abpvn_whitelist.tmp;copy src\abpvn_whitelist_elemhide.txt src\abpvn_whitelist_elemhide.tmp;"
 powershell -Command "Get-Content src\abpvn_general.tmp | Sort-Object | Get-Unique | Set-Content src\abpvn_general.txt;Get-Content src\abpvn_elemhide.tmp | Sort-Object | Get-Unique | Set-Content src\abpvn_elemhide.txt;Get-Content src\abpvn_whitelist.tmp | Sort-Object | Get-Unique | Set-Content src\abpvn_whitelist.txt; Get-Content src\abpvn_whitelist_elemhide.tmp | Sort-Object | Get-Unique | Set-Content src\abpvn_whitelist_elemhide.txt;"
@@ -10,5 +8,4 @@ rem Build file
 copy /y /b src\abpvn_title.tmp+src\abpvn_general.txt+src\abpvn_elemhide.txt+src\abpvn_whitelist.txt+src\abpvn_whitelist_elemhide.txt abpvn.txt
 copy /y /b src\abpvn_title.tmp+src\abpvn_general.txt+src\abpvn_whitelist.txt abpvn_noelemhide.txt
 DEL src\*.tmp
-powershell -Command "$git_comment=Select-String -Path src\abpvn_title.txt -Pattern \"(\s\[.+?\])\" -AllMatches | %%{ $_.Matches } | %%{ $_.Value }; $git_comment=\"Update or Fix\"+$git_comment; git add .; git commit -m $git_comment; git push;";
 rem sh ../pull.sh
---
+++ push.cmd
@@ -0,0 +1,12 @@
+rem Update first
+git pull
+rem Run Build filter for EasyList
+cd filter
+call build-push.cmd
+rem Run Build filter for VIP
+cd ..
+cd filter-bak
+call build-push.cmd
+cd ..
+powershell -Command "$git_comment=Select-String -Path filter\src\abpvn_title.txt -Pattern \"(\s\[.+?\])\" -AllMatches | %%{ $_.Matches } | %%{ $_.Value }; $git_comment=\"Update or Fix\"+$git_comment; git add .; git commit -m $git_comment; git push;";
+sh pull.sh
