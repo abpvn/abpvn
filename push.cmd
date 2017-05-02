5807d664aa10080cd1680ec23e3e37e8f0417b6a
ABVPN <donghoang.nguyen@outlook.com>
2017-05-02 15:02:43 +0700

    remove pull.sh call

--- push.cmd
+++ push.cmd
@@ -5,4 +5,3 @@ cd filter
 call build.cmd
 cd ..
 powershell -Command "$git_comment=Select-String -Path filter\src\abpvn_title.txt -Pattern \"(\s\[.+?\])\" -AllMatches | %%{ $_.Matches } | %%{ $_.Value }; $git_comment=\"Update or Fix\"+$git_comment; git add .; git commit -m $git_comment; git push;";
-sh pull.sh
