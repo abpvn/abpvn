45d897360cfb20ca9341e67dafca3ffbb9ddb49c
Hoang Rio <donghoang.nguyen@outlook.com>
2017-10-24 19:15:25 +0700

    Update or Fix [123link.io]

--- filter/build-push.cmd
+++ filter/build-push.cmd
@@ -1,3 +1,4 @@
 call build
 powershell -Command "$git_comment=Select-String -Path src\abpvn_title.txt -Pattern \"(\s\[.+?\])\" -AllMatches | %%{ $_.Matches } | %%{ $_.Value }; $git_comment=\"Update or Fix\"+$git_comment; git add .; git commit -m $git_comment; git push;";
+timeout 3
 cd .. && pull.sh
