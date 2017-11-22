40dbc592aff25eadb188384c467480b0c8d62753
hoangnd <donghoang.nguyen@outlook.com>
2017-11-22 10:05:36 +0700

    Update or Fix  [maclife.vn]

--- push.cmd
+++
@@ -1,7 +0,0 @@
-rem Update first
-git pull
-rem Run Build filter for EasyList
-cd filter
-call build.cmd
-cd ..
-powershell -Command "$git_comment=Select-String -Path filter\src\abpvn_title.txt -Pattern \"(\s\[.+?\])\" -AllMatches | %%{ $_.Matches } | %%{ $_.Value }; $git_comment=\"Update or Fix\"+$git_comment; git add .; git commit -m $git_comment; git push;";
