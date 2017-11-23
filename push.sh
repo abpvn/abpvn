77ec9c37260084ac6d891c8880b8ffd0aad1945d
hoangnd <donghoang.nguyen@outlook.com>
2017-11-23 13:14:15 +0700

    update sh

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd filter
 sh build.sh
-gitcomment="Update or Fix "$(cat src/abpvn_title.txt | grep -o '\s\[.*\]')
+gitcomment="Update or Fix"$(cat src/abpvn_title.txt | grep -o '\s\[.*\]')
 cd ..
 git add .
 git commit -m "$gitcomment"
