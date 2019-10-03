b7b56961cd6a52528dabfbd817a886d12f88143e
Hoàng Rio <donghoang.nguyen@outlook.com>
2019-10-03 18:33:55 +0700

    Update or Fix [thevangtv.com]

--- script/abpvn.js
+++ script/abpvn.js
@@ -15,8 +15,8 @@
 // @grant       GM_registerMenuCommand
 // @include     http://*
 // @include     https://*
-// @version     2.2.62
-// @change-log  Update home page and icon url to https
+// @version     2.2.63
+// @change-log  Remove fake link of https://banthang.tv/
 // @run-at      document-end
 // ==/UserScript==
 /* String Prototype */
@@ -447,7 +447,7 @@ var fixSite = {
             window.adblock = false;
             window.adblock2 = false;
             window.turnoff = true;
-            window.open = function () {};
+            window.open = function () { };
             //
             // @run-at document-end
             //
@@ -494,9 +494,9 @@ var fixSite = {
                         }
                     }, 100);
                 }
-                window.onclick = function () {};
-                document.onclick = function () {};
-                document.body.onclick = function () {};
+                window.onclick = function () { };
+                document.onclick = function () { };
+                document.body.onclick = function () { };
             });
         }
     },
@@ -507,12 +507,13 @@ var fixSite = {
         }
     },
     fakelinkRemover: function () {
-        if (this.url.startWith('https://ibongda.tv') || this.url.startWith('https://thevang.tv')) {
-            var fakeLink = document.querySelectorAll('a[data-href][rel="nofollow"]');
+        if (this.url.startWith('https://ibongda.tv') || this.url.startWith('https://thevang.tv') || this.url.startWith('https://banthang.tv/')) {
+            ABPVN.cTitle();
+            var fakeLink = document.querySelectorAll('a[data-href][rel="nofollow"],a[data-url][rel="nofollow"]');
             var count = 0;
             for (var i = 0; i < fakeLink.length; i++) {
                 if (fakeLink[i]) {
-                    fakeLink[i].setAttribute('href', fakeLink[i].getAttribute('data-href'));
+                    fakeLink[i].setAttribute('href', fakeLink[i].getAttribute('data-href') || fakeLink[i].getAttribute('data-url'));
                     count++;
                 }
             }
@@ -568,30 +569,30 @@ var fixSite = {
     },
     removeRedirect() {
         var configs = [{
-                url: 'https://samsungvn.com',
-                replace: 'https://samsungvn.com/xfa-interstitial/redirect?url=',
-            },
-            {
-                url: 'https://forum.vietdesigner.net',
-                replace: 'redirect/?url='
-            },
-            {
-                url: 'http://sinhvienit.net',
-                replace: 'http://sinhvienit.net/goto/?'
-            },
-            {
-                url: 'http://phanmemaz.com/',
-                replace: 'http://phanmemaz.com/wp-content/plugins/tm-wordpress-redirection/l.php?'
-            },
-            {
-                url: 'forums.voz.vn/showthread.php',
-                replace: '/redirect/index.php?link='
-            },
-            {
-                url: 'www.webtretho.com/forum/',
-                replace: /http(s?):\/\/webtretho\.com\/forum\/links\.php\?url=/,
-                selector: 'a[href*="webtretho.com/forum/links.php?url="]'
-            }
+            url: 'https://samsungvn.com',
+            replace: 'https://samsungvn.com/xfa-interstitial/redirect?url=',
+        },
+        {
+            url: 'https://forum.vietdesigner.net',
+            replace: 'redirect/?url='
+        },
+        {
+            url: 'http://sinhvienit.net',
+            replace: 'http://sinhvienit.net/goto/?'
+        },
+        {
+            url: 'http://phanmemaz.com/',
+            replace: 'http://phanmemaz.com/wp-content/plugins/tm-wordpress-redirection/l.php?'
+        },
+        {
+            url: 'forums.voz.vn/showthread.php',
+            replace: '/redirect/index.php?link='
+        },
+        {
+            url: 'www.webtretho.com/forum/',
+            replace: /http(s?):\/\/webtretho\.com\/forum\/links\.php\?url=/,
+            selector: 'a[href*="webtretho.com/forum/links.php?url="]'
+        }
         ];
         configs.forEach(function (config) {
             this.removeRedir(config);
@@ -681,7 +682,7 @@ var adBlocker = {
         if (this.url.startWith('http://phimnhanh.com/xem-phim')) {
             Logger.warn('Đã chặn video preload');
             if (video !== undefined) {
-                video.preroll = function (options) {};
+                video.preroll = function (options) { };
             }
         }
     },
