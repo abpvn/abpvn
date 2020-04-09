2e64199f9fd03d5c721c0578a1d16ba7d1c8f254
Hoàng Rio <donghoang.nguyen@outlook.com>
2020-04-09 08:47:38 +0700

    Update or Fix [mshare.xyz script]

--- script/abpvn.js
+++ script/abpvn.js
@@ -15,8 +15,8 @@
 // @grant       GM_registerMenuCommand
 // @include     http://*
 // @include     https://*
-// @version     2.2.72
-// @change-log  Update mshares.co domain
+// @version     2.2.73
+// @change-log  Update mshares.co,mshare.xyz domain
 // @run-at      document-end
 // ==/UserScript==
 /* String Prototype */
@@ -79,7 +79,7 @@ var byPass = {
         }
     },
     quickByPassLink: function () {
-        var regex = /123link\..*|phlame.pw|mshares\.co|megaurl\.*|licklink.net|www.123l\.*|vinaurl\.*|share4you.pro|doxeaz10.site|derow.win|linkviet.net|ez4linkss.com|ckk.ai/;
+        var regex = /123link\..*|phlame.pw|mshares\.co|mshare\.xyz|megaurl\.*|licklink.net|www.123l\.*|vinaurl\.*|share4you.pro|doxeaz10.site|derow.win|linkviet.net|ez4linkss.com|ckk.ai/;
         var largeTimeoutHost = /licklink.net|share4you.pro|derow.win/;
         var autoCaptchaOnlyList = /megaurl\.*|vinaurl\.*|doxeaz10.site|linkviet.net|ez4linkss.com|ckk.ai/;
         if (regex.test(location.hostname)) {
