a44791c27410f391b181570d206f0f0832b247dc
Hoàng Rio <donghoang.nguyen@outlook.com>
2021-06-05 09:56:18 +0700

    Update or Fix [motphimmoi.net]

--- filter/build.sh
+++ filter/build.sh
@@ -7,6 +7,7 @@ cp -r src/abpvn_ad_domain.txt src/abpvn_ad_domain.tmp
 cp -r src/abpvn_elemhide.txt src/abpvn_elemhide.tmp
 cp -r src/abpvn_whitelist.txt src/abpvn_whitelist.tmp
 cp -r src/abpvn_whitelist_elemhide.txt src/abpvn_whitelist_elemhide.tmp
+cp -r src/abpvn_ublock_specific.txt src/abpvn_ublock_specific.tmp
 # sort tmp to txt
 sort -u -o src/abpvn_adult.txt src/abpvn_adult.tmp
 sort -u -o src/abpvn_adult_elemhide.txt src/abpvn_adult_elemhide.tmp
@@ -15,6 +16,7 @@ sort -u -o src/abpvn_ad_domain.txt src/abpvn_ad_domain.tmp
 sort -u -o src/abpvn_elemhide.txt src/abpvn_elemhide.tmp
 sort -u -o src/abpvn_whitelist.txt src/abpvn_whitelist.tmp
 sort -u -o src/abpvn_whitelist_elemhide.txt src/abpvn_whitelist_elemhide.tmp
+sort -u -o src/abpvn_ublock_specific.txt src/abpvn_ublock_specific.tmp
 # make time stamp update
 TIME_STAMP=`date +'%d %b %Y %H:%M'`
 VERSION=`date +'%Y%m%d%H%M'`
@@ -24,6 +26,7 @@ echo >> src/abpvn_title.tmp
 fi
 # add to 1 file
 cat src/abpvn_title.tmp src/abpvn_general.txt src/abpvn_ad_domain.txt src/abpvn_elemhide.txt src/abpvn_whitelist.txt src/abpvn_whitelist_elemhide.txt src/abpvn_adult.txt src/abpvn_adult_elemhide.txt > abpvn.txt
+cat src/abpvn_title.tmp src/abpvn_general.txt src/abpvn_ad_domain.txt src/abpvn_elemhide.txt src/abpvn_whitelist.txt src/abpvn_whitelist_elemhide.txt src/abpvn_adult.txt src/abpvn_adult_elemhide.txt src/abpvn_ublock_specific.txt > abpvn_ublock.txt
 cat src/abpvn_title.tmp src/abpvn_general.txt src/abpvn_ad_domain.txt src/abpvn_whitelist.txt src/abpvn_adult.txt > abpvn_noelemhide.txt
 # remove tmp file
 rm -rf src/*.tmp
