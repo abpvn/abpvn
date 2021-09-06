2d21c8b962af1c12a0e0bfa262e1777ebabf4590
Hoàng Rio <donghoang.nguyen@outlook.com>
2021-09-06 11:30:24 +0700

    A: abpvn.com

--- commit.sh
+++ commit.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 cd filter
 sh build.sh
-gitcomment="Update or Fix"$(cat src/abpvn_title.txt | grep -o '\s\[.*\]')
+commit_type="A"
+read -p "Is this commit to update exist site? (Y/N) " is_update
+if [ $is_update == [yY] ]
+then
+    commit_type="M"
+fi
+read -p "Enter process domain? Ex: example.com " domain
+gitcomment="$commit_type: $domain"
 cd ..
 git add .
 git commit -m "$gitcomment"
