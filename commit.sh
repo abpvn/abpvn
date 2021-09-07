89728b6925048ec6700168773544b5fd2e7230ac
Hoàng Rio <donghoang.nguyen@outlook.com>
2021-09-07 08:39:51 +0700

    M: Remove oconflict line

--- commit.sh
+++ commit.sh
@@ -3,7 +3,7 @@ cd filter
 sh build.sh
 commit_type="A"
 read -p "Is this commit to update exist site? (Y/N) " is_update
-if [ $is_update == [yY] ]
+if [ $is_update == 'y' ] || [ $is_update == 'Y' ]
 then
     commit_type="M"
 fi
