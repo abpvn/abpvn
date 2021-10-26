12130c7a04304abfa920deced1c1f347566bfc1c
Hoàng Rio <donghoang.nguyen@outlook.com>
2021-10-26 07:44:22 +0700

    D: vphimz.net

--- commit.sh
+++ commit.sh
@@ -2,10 +2,13 @@
 cd filter
 sh build.sh
 commit_type="A"
-read -p "Is this commit to update exist site? (Y/N) " is_update
-if [ $is_update == 'y' ] || [ $is_update == 'Y' ]
+read -p "Enter filter update type Add (A), Modified (M), Delete (D)? " update_type
+if [ $update_type == 'm' ] || [ $update_type == 'M' ]
 then
     commit_type="M"
+elif [ $update_type == 'd' ] || [ $update_type == 'D' ]
+then
+    commit_type="D"
 fi
 read -p "Enter process domain? Ex: example.com " domain
 gitcomment="$commit_type: $domain"
