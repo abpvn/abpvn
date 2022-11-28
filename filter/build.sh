1c382396e90ffe21b2247b8f141c86833f812c93
Hoàng Rio <donghoang.nguyen@outlook.com>
2022-11-28 19:32:32 +0700

    Use $ intead of \n to remove grep warning

--- filter/build.sh
+++ filter/build.sh
@@ -33,12 +33,11 @@ cat src/abpvn_title.tmp src/abpvn_general.txt src/abpvn_ad_domain.txt src/abpvn_
 sed -e '/^$/d' abpvn.tmp > abpvn.txt
 # abpvn_ublock.txt
 cat src/abpvn_title.tmp src/abpvn_general.txt src/abpvn_ad_domain.txt src/abpvn_elemhide.txt src/abpvn_whitelist.txt src/abpvn_whitelist_elemhide.txt src/abpvn_adult.txt src/abpvn_adult_elemhide.txt src/abpvn_ublock_specific.txt > abpvn_ublock.tmp
-# Search useless rule by ublock specific
 function get_replace_rule() {
     # $1 is input file
     local INPUT_FILE="src/$1.txt"
     local REMOVE_RULE_TMP_FILE="$1_removal.tmp"
-    grep -E '^-(.*)\n' $INPUT_FILE > $REMOVE_RULE_TMP_FILE
+    grep -E '^-(.*)$' $INPUT_FILE > $REMOVE_RULE_TMP_FILE
     local REPLACE_RULE=''
     for removeRule in $(cat $REMOVE_RULE_TMP_FILE | grep -v '\.$'); do
             printf -v removeRule "%q\n" $removeRule
@@ -52,6 +51,7 @@ function get_replace_rule() {
     done
     echo $REPLACE_RULE
 }
+# Search useless rule by ublock specific
 UBLOCK_REPLACE_RULE=$(get_replace_rule abpvn_ublock_specific)
 if [ "$UBLOCK_REPLACE_RULE" == "" ]
 then
@@ -63,6 +63,7 @@ else
 fi
 # abpvn_adguard.txt
 cat src/abpvn_title.tmp src/abpvn_general.txt src/abpvn_ad_domain.txt src/abpvn_elemhide.txt src/abpvn_whitelist.txt src/abpvn_whitelist_elemhide.txt src/abpvn_adult.txt src/abpvn_adult_elemhide.txt src/abpvn_adguard_specific.txt > abpvn_adguard.tmp
+# Search useless rule by adguard specific
 ADGUARD_REPLACE_RULE=$(get_replace_rule abpvn_adguard_specific)
 if [ "$ADGUARD_REPLACE_RULE" == "" ]
 then
@@ -77,6 +78,7 @@ cat src/abpvn_title.tmp src/abpvn_general.txt src/abpvn_ad_domain.txt src/abpvn_
 sed -e '/^$/d' abpvn_noelemhide.tmp > abpvn_noelemhide.txt
 # abpvn_content_blocker.txt
 cat src/abpvn_title.tmp src/abpvn_general.txt src/abpvn_ad_domain.txt src/abpvn_elemhide.txt src/abpvn_whitelist.txt src/abpvn_whitelist_elemhide.txt src/abpvn_adult.txt src/abpvn_adult_elemhide.txt src/abpvn_content_blocker_hot_fix.txt > abpvn_content_blocker.tmp
+# Search useless rule by content blocker
 CONTENT_BLOCKER_REPLACE_RULE=$(get_replace_rule abpvn_content_blocker_hot_fix)
 if [ "$CONTENT_BLOCKER_REPLACE_RULE" == "" ]
 then
