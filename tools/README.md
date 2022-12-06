bb0300270826b96b222633a557342ddf685c0ec6
Hoàng Rio <donghoang.nguyen@outlook.com>
2022-12-06 13:32:00 +0700

    Add tools folder

---
+++ tools/README.md
@@ -0,0 +1,4 @@
+## ABPVN Tools with python (3.8) language
+
+### Tool list:
+* Domain change detect: [domain_change](domain_change)
---
+++ tools/domain_change/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='domain_change',
+    version='0.0.1',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+)
