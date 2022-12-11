be2bbd41e750a1836aedb0e98399d78fa08c67ea
Hoàng Rio <donghoang.nguyen@outlook.com>
2022-12-11 23:03:24 +0700

    Add more console and reject ending

--- tools/domain_change.py
+++ tools/domain_change.py
@@ -9,7 +9,8 @@ class DomainChange():
         Check domain change and return pair of change domain
         """
         redirect_pairs = []
-        for domain in domains:
+        total_domain = len(domains)
+        for index, domain in enumerate(domains):
             redirect_pair, is_error = DomainChange.__get_redirect_domain(
                 domain)
             if is_error:
@@ -17,6 +18,7 @@ class DomainChange():
                     "-----------------Check domain {} got error-----------".format(domain))
             if redirect_pair is not False and len(redirect_pair) == 2:
                 redirect_pairs.append(redirect_pair)
+            print("Processed {}/{} domain remaing".format(index, total_domain))
         print("Found {} domain changed with redirect".format(len(redirect_pairs)))
         return redirect_pairs
 
@@ -30,13 +32,15 @@ class DomainChange():
             request_url = "http://" + domain
             res = requests.head(url=request_url, headers={
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0'
-            }, allow_redirects=True)
+            }, allow_redirects=True, timeout=2)
             if domain in res.url:
                 return (False, False)
             final_redirect_domain = res.url.replace('https://', '').replace(
                 'http://', '').replace('/', '').replace('www.', '').replace('www1.', '')
-            print("----------Domain {} redirected to {}----------".format(domain,
+            print("|---------------------------------------------|")
+            print("|----------Domain {} redirected to {}---------|".format(domain,
                   final_redirect_domain))
+            print("|---------------------------------------------|")
             return ([domain, final_redirect_domain], False)
         except Exception as ex:
             print("Got exception {}".format(ex))
