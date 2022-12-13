46f7f45ef2c44a347d02617130a03534435dca38
Hoàng Rio <donghoang.nguyen@outlook.com>
2022-12-13 16:20:09 +0700

    M: blogtruyen.vn

--- tools/__main__.py
+++ tools/__main__.py
@@ -1,14 +1,22 @@
 from domain_list import DomainList
 from domain_change import DomainChange
 from pprint import pprint
+from datetime import datetime
 
 
 def main():
+    start_time = datetime.now()
+    print("Script start at: {0}\n".format(start_time))
     f = open("../filter/abpvn_ublock.txt", "r", encoding="utf8")
     filter_text = f.read()
     domains = DomainList.get_all_domain(filter_text)
-    result = DomainChange.check_domain_change(domains)
+    domain_change = DomainChange(domains)
+    result = domain_change.check_domain_change()
     pprint(result)
+    end_time = datetime.now()
+    print("Script finish at: {0}\n".format(end_time))
+    running_time = end_time - start_time
+    print("Running in {0} seconds".format(running_time.total_seconds()))
 
 
 main()
--- tools/domain_change.py
+++ tools/domain_change.py
@@ -1,37 +1,26 @@
 import requests
 from const import Const
 import re
+import threading
 
-class DomainChange():
-    @staticmethod
-    def check_domain_change(domains):
-        """
-        Check domain change and return pair of change domain
-        """
-        redirect_pairs = []
-        total_domain = len(domains)
-        for index, domain in enumerate(domains):
-            if domain in Const.SKIP_CHECK_REDIRECT:
-                # Skip if domain in SKIP_CHECK_REDIRECT list
-                print("Skiped domain {}".format(domain))
-                continue
-            redirect_pair, is_error = DomainChange.__get_redirect_domain(
-                domain)
-            if is_error:
-                print(
-                    "-----------------Check domain {} got error-----------".format(domain))
-            if redirect_pair is not False and len(redirect_pair) == 2:
-                redirect_pairs.append(redirect_pair)
-            print("Processed {}/{} domain".format(index + 1, total_domain))
-        print("Found {} domain changed with redirect".format(len(redirect_pairs)))
-        return redirect_pairs
 
-    @staticmethod
-    def __get_redirect_domain(domain):
+class DomainCheck(threading.Thread):
+    def set_data(self, domain, redirect_pairs, index, total_domain):
+        """
+        Set process domain
+        """
+        self.__domain = domain
+        self.redirect_pairs = redirect_pairs
+        self.__domain_index = index
+        self.__total_domain = total_domain
+
+    def get_redirect_domain(self):
         """
         Request and get final redirect url. If same as input return False else return pair [source, redirection]
         """
-        print("Start check redirect of domain {}".format(domain))
+        if self.__domain is None:
+            return
+        domain = self.__domain
         try:
             request_url = "http://" + domain
             res = requests.head(url=request_url, headers={
@@ -43,12 +32,50 @@ class DomainChange():
                     final_redirect_domain = matches[0]
                     print(
                         "|-----------------------------------------------------------------------------|")
-                    print("|----------Domain {} redirected to {}---------|".format(domain,
-                        final_redirect_domain))
+                    message = "Domain {} redirected to {}".format(domain,
+                        final_redirect_domain)
+                    space_fill = (77 - len(message)) / 2
+                    for i in range(space_fill):
+                        message = " " + message + " "
+                    print("|{}|".format(message))
                     print(
                         "|-----------------------------------------------------------------------------|")
-                    return ([domain, final_redirect_domain], False)
-            return (False, False)
+                    self.lock.acquire()
+                    self.redirect_pairs.append([domain, final_redirect_domain])
+                    self.lock.release()
+                    print("{}: Processed domain in index {}/{}".format(domain, self.__domain_index, self.__total_domain))
+            else:
+                print("{}: is not redirect".format(domain))
         except Exception as ex:
-            print("Got exception {}".format(ex))
-            return (False, True)
+            print("{}: Got exception {} when check".format(domain, ex))
+
+    def run(self):
+        self.lock = threading.Lock()
+        self.get_redirect_domain()
+class DomainChange():
+    def __init__(self, domains) -> None:
+        self.domains = domains
+        self.__threads = []
+        self.redirect_pairs = []
+
+    def process_domain(self, domain, index, total_domain):
+        domain_check = DomainCheck()
+        domain_check.set_data(domain, self.redirect_pairs, index, total_domain)
+        self.__threads.append(domain_check)
+        domain_check.start()
+
+    def check_domain_change(self):
+        """
+        Check domain change and return pair of change domain
+        """
+        total_domain = len(self.domains)
+        for index, domain in enumerate(self.domains):
+            if domain in Const.SKIP_CHECK_REDIRECT:
+                # Skip if domain in SKIP_CHECK_REDIRECT list
+                print("{}: Skiped domain".format(domain))
+                continue
+            self.process_domain(domain, index, total_domain)
+        for t in self.__threads:
+            t.join()
+        print("----Found {} domain changed with redirect----".format(len(self.redirect_pairs)))
+        return self.redirect_pairs
