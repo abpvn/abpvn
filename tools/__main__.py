5715b5694afd360cf6a5e9f233c27875fa229cf1
Hoàng Rio <donghoang.nguyen@outlook.com>
2022-12-14 15:06:20 +0700

    Update tools

--- tools/__main__.py
+++ tools/__main__.py
@@ -11,12 +11,15 @@ def main():
     filter_text = f.read()
     domains = DomainList.get_all_domain(filter_text)
     domain_change = DomainChange(domains)
-    result = domain_change.check_domain_change()
-    pprint(result)
+    redirect_pairs, error_domains = domain_change.check_domain_change()
+    print("----Found {} domain changed with redirect----".format(len(redirect_pairs)))
+    pprint(redirect_pairs)
+    print("----Found {} error domain----".format(len(error_domains)))
+    pprint(error_domains)
     end_time = datetime.now()
     print("Script finish at: {0}\n".format(end_time))
     running_time = end_time - start_time
     print("Running in {0} seconds".format(running_time.total_seconds()))
 
-
-main()
+if __name__ == '__main__':
+    main()
