fd05beb312083906df77ba4ebfc6568646687f95
Hoàng Rio <donghoang.nguyen@outlook.com>
2022-12-14 09:08:51 +0700

    Update tool from single

--- tools/const.py
+++ tools/const.py
@@ -12,5 +12,5 @@ class Const():
         'amazonaws.com', 'blogspot.com', 'blogtruyen.com', 'fptplay.net',
         'doubleclick.net', 'github.io', 'herokuapp.com', 'zing.vn', 'com.vn',
         'net.vn', 'edu.vn', 'googlesyndication.com', 'gov.vn', 'nct.vn', 'org.vn',
-        'phukienthoitranggiare.com'
+        'phukienthoitranggiare.com', 'cloudfront.net'
     ]
