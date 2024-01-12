17a73d573cafa255a1ebb1d705e4b2d0fa51bf63
Hoàng Rio <donghoang.nguyen@outlook.com>
2024-01-12 09:04:22 +0700

    A: Inital diff patch with source from uBlockOrigin/uAssets

--- commit.sh
+++ commit.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+VERSION=`date +'%Y%m%d%H%M%S'`
 cd filter
-bash build.sh
-commit_type="A"
-read -p "Enter filter update type Add (A), Modified (M), Delete (D)? " update_type
-if [ $update_type == 'm' ] || [ $update_type == 'M' ]
-then
-    commit_type="M"
-elif [ $update_type == 'd' ] || [ $update_type == 'D' ]
-then
-    commit_type="D"
-fi
-read -p "Enter process domain? Ex: example.com " domain
-gitcomment="$commit_type: $domain"
+bash build.sh "$VERSION"
 cd ..
-git add .
-git commit -m "$gitcomment"
+PATCHES_DIR="filter/patches"
+bash make-diffpatch.sh "$VERSION" "$PATCHES_DIR"
+SKIP_COMMIT=$1
+if [[ -z $SKIP_COMMIT ]]; then
+    commit_type="A"
+    read -p "Enter filter update type Add (A), Modified (M), Delete (D)? " update_type
+    if [ $update_type == 'm' ] || [ $update_type == 'M' ]
+    then
+        commit_type="M"
+    elif [ $update_type == 'd' ] || [ $update_type == 'D' ]
+    then
+        commit_type="D"
+    fi
+    read -p "Enter process domain? Ex: example.com " domain
+    gitcomment="$commit_type: $domain"
+    git add .
+    git commit -m "$gitcomment"
+fi
---
+++ make-diffpatch.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+#
+# This script assumes a linux environment
+# ORIGINAL SOURCE FROM: https://github.com/uBlockOrigin/uAssets/blob/master/tools/make-diffpatch.sh
+
+set -e
+
+# To be executed at the root of CDN repo
+#
+# It's not being hosted at CDN because that
+# repo is also used as a website
+
+VERSION=$1
+if [[ -z $VERSION ]]; then
+    echo "Error: No version provided, aborting"
+    exit 1
+fi
+
+PATCHES_DIR=$2
+if [[ -z $PATCHES_DIR ]]; then
+    echo "Error: patches directory is not provided, aborting"
+    exit 1
+fi
+
+PREVIOUS_VERSION=$(<version)
+PREVIOUS_PATCH_FILE="$PATCHES_DIR/$PREVIOUS_VERSION.patch"
+: > "$PREVIOUS_PATCH_FILE"
+
+NEXT_PATCH_FILE="$PATCHES_DIR/$VERSION.patch"
+
+# Temporary file to receive the RCS patch data
+DIFF=$(mktemp)
+
+FILES=( $(git diff --name-only) )
+for FILE in "${FILES[@]}"; do
+
+    # Reference:
+    # https://github.com/ameshkov/diffupdates
+
+    if (head "$FILE" | grep -q '^! Version: '); then
+        sed -Ei "1,10s;^! Version: .+$;! Version: $VERSION;" "$FILE"
+    fi
+
+    # Patches are for filter lists supporting differential updates
+    if (head "$FILE" | grep -q '^! Diff-Path: '); then
+
+        # Extract diff name from `! Diff-Path:` field
+        DIFF_NAME=$(grep -m 1 -oP '^! Diff-Path: [^#]+#?\K.*' "$FILE")
+        # Fall back to `! Diff-Name:` field if no name found
+        # Remove once `! Diff-Name:` is no longer needed after transition
+        if [[ -z $DIFF_NAME ]]; then
+            DIFF_NAME=$(grep -m 1 -oP '^! Diff-Name: \K.+' "$FILE")
+        fi
+        echo "Info: Diff name for ${FILE} is ${DIFF_NAME}"
+
+        # We need a patch name to generate a valid patch
+        if [[ -n $DIFF_NAME ]]; then
+
+            # Compute relative patch path
+            PATCH_PATH="$(realpath --relative-to="$(dirname "$FILE")" "$NEXT_PATCH_FILE")"
+
+            # Fill in patch path to next version (do not clobber hash portion)
+            sed -Ei "1,10s;^! Diff-Path: [^#]+(#.+)?$;! Diff-Path: $PATCH_PATH\1;" "$FILE"
+
+            # Compute the RCS diff between current version and new version
+            git show "HEAD:$FILE" | diff -n - "$FILE" > "$DIFF" || true
+
+            FILE_CHECKSUM="$(sha1sum "$FILE")"
+            FILE_CHECKSUM=${FILE_CHECKSUM:0:10}
+
+            DIFF_LINES=$(wc -l < "$DIFF")
+            echo "Info: Computed patch for ${FILE} has ${DIFF_LINES} lines"
+
+            # Populate output file with patch information
+            echo "Info: Adding patch data of ${FILE} to ${PREVIOUS_PATCH_FILE}"
+            echo "diff name:$DIFF_NAME lines:$DIFF_LINES checksum:$FILE_CHECKSUM" >> "$PREVIOUS_PATCH_FILE"
+            cat "$DIFF" >> "$PREVIOUS_PATCH_FILE"
+
+        else
+
+            echo "Error: Diff name not found, skipping"
+
+        fi
+    fi
+
+    # Stage changed file
+    echo "Info: Staging $FILE"
+    git add -u "$FILE"
+
+done
+
+# Create a patch only if there was a previous version
+if [[ -n $PREVIOUS_VERSION ]]; then
+    echo "Info: Staging $PREVIOUS_PATCH_FILE"
+    git add "$PREVIOUS_PATCH_FILE"
+fi
+
+echo -n "$VERSION" > version
+git add version
+
+rm -f "$DIFF"
