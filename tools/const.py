fb84342ebcfc176fa01feedbbf1c4edb476ea050
Hoàng Rio <donghoang.nguyen@outlook.com>
2024-01-12 11:49:47 +0700

    Add .patch to domain reject ending

--- tools/const.py
+++ tools/const.py
@@ -12,7 +12,7 @@ class Const():
         'firstmessfloadright.samItem', 'banner-top-box.click',
         'clear.hd', 'info.card', 'left.box', 'right.box',
         'blqPr.nwsItHm', 'text-center.adsense', '.btn', '.show',
-        'show.fade', 'parent.special', 'flex-1.table'
+        'show.fade', 'parent.special', 'flex-1.table', '.patch'
     ]
     SKIP_CHECK_REDIRECT = [
         'amazonaws.com', 'blogspot.com', 'blogtruyen.com', 'fptplay.net',
