2dd19235f0f5d895b66432b21e744087d7e2134e
Hoàng Rio <hi@hoangnguyendong.dev>
2025-12-16 03:22:42 +0000

    tools: add .php to domain reject ending

--- tools/const.py
+++ tools/const.py
@@ -10,7 +10,7 @@ class Const():
         'abpvn.org', '.html', '.button',
         '.modal', '.ico', '.write', '.webp', '.btn', '.show', 'parent.special', '.patch', 'li.special.parent',
         '2Flh3.googleusercontent.com', 'div.ad',
-        'ibyteimg.com', '.fixed', '.blpha'
+        'ibyteimg.com', '.fixed', '.blpha', '.php'
     ]
     SKIP_CHECK_REDIRECT = [
         'blogspot.com', 'fptplay.net', 'doubleclick.net', 'com.vn',
